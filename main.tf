4974f2d7c610697c8971d3403a0ce32bb189b7a3
Nick Coleman <hsart1001@gmail.com>
2026-02-23 13:54:08 +0000

    Add EBS-backed Mongo + Mongo Express and tidy workflow

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "ssh_my_ip" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["90.249.83.220/32"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
@@ -36,7 +36,7 @@ resource "aws_security_group" "ssh_my_ip" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["90.249.83.220/32"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
@@ -44,7 +44,7 @@ resource "aws_security_group" "ssh_my_ip" {
     from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
-    cidr_blocks = ["90.249.83.220/32"]
+    cidr_blocks = [var.my_ip]
   }
 
   egress {
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "my_ip" {
+  description = "Your public IP in CIDR format (e.g. 1.2.3.4/32)"
+  type        = string
+}
